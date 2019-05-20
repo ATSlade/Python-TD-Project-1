66e84a6097297229e2e3a7287a2d6701cf8139a0
Slade <ATSlade@users.noreply.github.com>
2019-05-20 09:21:50 -0400

    Update THTD-Python-P1-Number_Guessing_Game_v2_1.py
    Added objective and comments

--- THTD-Python-P1-Number_Guessing_Game_v2_1.py
+++ THTD-Python-P1-Number_Guessing_Game_v2_1.py
@@ -1,4 +1,5 @@
 # Acie Slade - # Project 1 (Number Guessing Game)
+# Objective: Exceeds Expectations
 
 import random
 
@@ -17,43 +18,49 @@ At the prompt, you can enter 'x' to exit/quit, if you'd like.
 ''') 
 
 def start_game():
+# SET-UP
     play = True
     best_score = 10
     guess_count = 0
     rando = random.randint(1, 10) # RANDOM NUMBER GEN referenced http://pythonspot.com/random-numbers/
     print("\nOk...I have my secret number.\n")    
-    
+# MAIN_CODE    
     while play is True:
-        player_guess = input("What's your guess?: ")
+        player_guess = input("What's your guess?: \n")
+#EXIT/QUIT
         if player_guess.lower() == 'x':
-            print("\nOk...good-bye!")
+            print("\nOk...good-bye!\n")
             break
         else:
             try:
                 player_guess = int(player_guess)
             except ValueError:
-                print("Invalid entry. Please choose an INTEGER NUMBER between and including 1 and 10: ")
+                print("Invalid entry. That guess will not count against you. Please choose an INTEGER between and including 1 and 10: ")
                 continue
             guess_count += 1    
+# THE GUESS (PART I)
             if player_guess == rando:
                     if guess_count < best_score:
                         best_score = guess_count 
                         print("Got it! And we have a new best score of {} tries.\n".format(guess_count))
                     else:
                         print("Got it! It only took {} guesses, but you did not beat the best score.\n".format(guess_count))
+# PLAY AGAIN or QUIT
                     play_again = input("Would you like to play again? 'y' for yes, 'n' for no: ")                   
                     if play_again.lower() == 'y':
                         guess_count = 0
-                        rando = random.randint(1, 10)                            
+                        rando = random.randint(1, 10)
+                        print("\nYes! We're playing on. The score to beat is {} guesses.".format(best_score))                            
                     else:                        
                         print("Okie dokie then...good-bye!\n")
                         break                
+# THE GUESS (PART II)
             elif player_guess > rando and player_guess <= 10:
                 print("The secret number is lower. Please try again.")
             elif player_guess < rando and player_guess >= 1:
                 print("The secret number is higher. Please try again.")
             else:
-                print("That guess is outside the range and won't count against you. Please try again.")
+                print("That guess is outside the range and won't count against you. Please guess a number between and including 1 and 10.")
                 guess_count -= 1   
 
 if __name__ == '__main__':
